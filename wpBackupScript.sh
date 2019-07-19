cd76a42e9882786fb7d1db45859e43f153cc379d
Alexander J. Gray <alexgray410@gmail.com>
2019-07-19 15:03:29 -0400

    Update wpBackupScript.sh
    Add date variable to append date to filenames.

--- wpBackupScript.sh
+++ wpBackupScript.sh
@@ -2,8 +2,11 @@
 #Script creates tarball of WP site files and mysqldump of the WP Database
 #create tarball
 
- tar -C /var/www/html -czvf [pathTo]/[yourBackupDirectory]/wp-files.tar.gz [yourSiteDirectory]
+#Set date variables to append date to files
+today=`date '+%Y_%m_%d__%H_%M_%S'`;
+
+tar -C /var/www/html -czvf [pathTo]/[yourBackupDirectory]/$today.wp-files.tar.gz [yourSiteDirectory]
 
 #create the mysqldump of the wp-database
 
- mysqldump -u [username] -p'[yourPassword]' [db name] > [pathTo]/[yourBackupDirectory]/db.wp__backup.sql
+mysqldump -u [username] -p'[yourPassword]' [db name] > [pathTo]/[yourBackupDirectory]/$today.db.wp__backup.sql
